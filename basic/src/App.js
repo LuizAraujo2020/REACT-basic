149ce813caf9c74579584273e218981c9f73f81b
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-02 10:51:42 -0300

    button component example

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,10 +1,10 @@
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-        <h1>
-          Olá, mundo! Asdashuda
-        </h1>
+        <h1>Olá, mundo! React...</h1>
+        <Button title="Entrar"/>
     </div>
   );
 }
